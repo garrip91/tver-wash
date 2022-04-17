80f507522d5f2872fbfb8efa08d144c90387b20c
garrip91 <45920127+garrip91@users.noreply.github.com>
2022-04-17 21:11:42 +0300

    Summary

--- src/TverWashProject/urls.py
+++ src/TverWashProject/urls.py
@@ -16,6 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from TverWashApp.views import HomePageView
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+
 urlpatterns = [
+    path('', HomePageView.as_view(), name='/'), # TverWash
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
