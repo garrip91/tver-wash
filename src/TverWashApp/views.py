7be6248a32d800e8500b8e6b38c19c3e94a003c4
garrip91 <45920127+garrip91@users.noreply.github.com>
2022-04-17 21:35:04 +0300

    Summary

--- src/TverWashApp/views.py
+++ src/TverWashApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from django.views import View
+
+
+
 # Create your views here.
+class HomePageView(View):
+
+    def get(self, request):
+        #user_form = UserForm()
+        print(F'request.path == {self.request.path}')
+        return render(request, 'TverWashApp/index.html', context={})
