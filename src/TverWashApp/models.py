658e66e9390db6511bf2ebd72e19fa9a869bdccd
garrip91 <garrip91@gmail.com>
2022-04-22 09:01:38 +0300

    Summary

--- src/TverWashApp/models.py
+++ src/TverWashApp/models.py
@@ -10,49 +10,77 @@ from django.db import models
 
 
 class Services(models.Model):
-    image = models.ImageField(verbose_name='Фото', upload_to='TverWashApp/static/img/', null=False)
+
     name = models.CharField(max_length=30, verbose_name='Название услуги')
+    image = models.ImageField(verbose_name='Фото', upload_to='TverWashApp/static/img/', null=False)
     about = models.TextField(max_length=1000, verbose_name='Описание')
+
+    def __str__(self):
+        return str(self.name)
 
     class Meta:
         verbose_name = 'Услуги'
         verbose_name_plural = 'Услуги'
+        ordering = ['id']
 
 
 class Gallery(models.Model):
+
     image = models.ImageField(verbose_name='Фото для галереи', upload_to='TverWashApp/static/img/', null=False)
+
+    def __str__(self):
+        return str(self.id)
 
     class Meta:
         verbose_name = 'Галерея'
         verbose_name_plural = 'Галерея'
+        ordering = ['id']
 
 
 class Team(models.Model):
-    image = models.ImageField(verbose_name='Фото', upload_to='TverWashApp/static/img/')
+
     name = models.CharField(max_length=30, verbose_name='Имя')
+    image = models.ImageField(verbose_name='Фото', upload_to='TverWashApp/static/img/')
     about = models.TextField(max_length=1000, verbose_name='Краткая история')
-    number = models.IntegerField(verbose_name='Телефон', null=True)
+    #number = models.IntegerField(verbose_name='Телефон', null=True)
+    phone = models.CharField(max_length=18, verbose_name='Телефон', blank=True, null=True)
+
+    def __str__(self):
+        return str(self.name)
 
     class Meta:
         verbose_name = 'Команда'
         verbose_name_plural = 'Команда'
+        ordering = ['id']
 
 
 class Call_Back(models.Model):
+
     name = models.CharField(max_length=30, verbose_name='Имя')
-    number = models.IntegerField(verbose_name='Телефон')
+    #number = models.IntegerField(verbose_name='Телефон')
+    phone = models.CharField(max_length=18, verbose_name='Телефон', blank=True, null=True)
+
+    def __str__(self):
+        return str(self.name)
 
     class Meta:
         verbose_name = 'Заявки на звонок'
         verbose_name_plural = 'Заявки на звонок'
+        ordering = ['id']
 
 
 class Sign_Up(models.Model):
+
     name = models.CharField(max_length=30, verbose_name='Имя')
-    number = models.IntegerField(verbose_name='Телефон')
+    #number = models.IntegerField(verbose_name='Телефон')
+    phone = models.CharField(max_length=18, verbose_name='Телефон', blank=True, null=True)
     address = models.TextField(max_length=100, verbose_name='Адрес')
     wishes = models.TextField(max_length=1000, verbose_name='Пожелания')
+
+    def __str__(self):
+        return str(self.name)
 
     class Meta:
         verbose_name = 'Заявки на запись'
         verbose_name_plural = 'Заявки на запись'
+        ordering = ['id']
