a04286087ab27c410473cc64898f8e5cef8e1675
garrip91 <45920127+garrip91@users.noreply.github.com>
2022-04-23 22:25:46 +0300

    Summary

--- src/TverWashProject/settings.py
+++ src/TverWashProject/settings.py
@@ -49,7 +49,7 @@ ROOT_URLCONF = 'TverWashProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],#[BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates']
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,27 +104,29 @@ USE_I18N = True
 USE_TZ = True
 
 
-#STATIC_URL = '/static/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_URL = '/static/'
 # ДЛЯ РАЗРАБОТКИ:
-#STATICFILES_DIRS = [
-#    BASE_DIR / 'static'
-#]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 # # ДЛЯ ХОСТИНГА:
 # STATIC_ROOT = BASE_DIR / 'static'
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'src/TverWashApp'),
-)
-
-#EMAIL_BACKEND = Yandex_config.EMAIL_BACKEND
-#EMAIL_HOST = Yandex_config.EMAIL_HOST
-#EMAIL_PORT = Yandex_config.EMAIL_PORT
-#EMAIL_HOST_USER = Yandex_config.EMAIL_HOST_USER
-#EMAIL_HOST_PASSWORD = Yandex_config.EMAIL_HOST_PASSWORD
-#EMAIL_USE_TLS = Yandex_config.EMAIL_USE_TLS
-#EMAIL_USE_SSL = Yandex_config.EMAIL_USE_SSL
+EMAIL_BACKEND = Yandex_config.EMAIL_BACKEND
+EMAIL_HOST = Yandex_config.EMAIL_HOST
+EMAIL_PORT = Yandex_config.EMAIL_PORT
+EMAIL_HOST_USER = Yandex_config.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = Yandex_config.EMAIL_HOST_PASSWORD
+EMAIL_USE_TLS = Yandex_config.EMAIL_USE_TLS
+EMAIL_USE_SSL = Yandex_config.EMAIL_USE_SSL
