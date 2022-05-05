9117ccef826c0cb5df0bf2d7c773c045bc7fb859
garrip91 <garrip91@gmail.com>
2022-05-05 15:07:03 +0300

    Summary

--- src/TverWashApp/!mixins.py
+++ src/TverWashApp/!mixins.py
@@ -1,11 +1,11 @@
 from django.views.generic import View
-from .models import *
-from .forms import FeedbackForm, AppointmentForm, FooterFeedbackForm
-from django.contrib import messages
-from django.http import HttpResponseRedirect
+from .models import Call_Request, Call_Appointment
+from .forms import CallRequestForm, CallAppointmentForm
+#from django.contrib import messages
+#from django.http import HttpResponseRedirect
 
-from django.core.mail import send_mail
-from django.http import HttpResponseNotFound
+#from django.core.mail import send_mail
+#from django.http import HttpResponseNotFound
 
 
 class MyFormMixin1(View):
@@ -74,39 +74,6 @@ class MyFormMixin2(View):
                 return HttpResponseRedirect(self.request.path)
         else:
             self.appointment_form = AppointmentForm()
-        #####################################################################################
-        # print(F'request.path == {request.path}')
-        return super().dispatch(request, *args, **kwargs)
-
-
-class MyFormMixin3(View):
-
-    def dispatch(self, request, *args, **kwargs):
-        #################### РЕАЛИЗУЕМ ПОЛУЧЕНИЕ ЗАЯВКИ ОТ ПОЛЬЗОВАТЕЛЯ: ####################
-        if request.method == 'POST' and 'feedback3' in request.POST:
-            footer_feedback_form = FooterFeedbackForm(request.POST)
-            if footer_feedback_form.is_valid():
-                FooterFeedback_name = footer_feedback_form.cleaned_data.get('Feedback_name')
-                print(FooterFeedback_name)
-                FooterFeedback_phone = footer_feedback_form.cleaned_data.get('Feedback_phone')
-                print(FooterFeedback_phone)
-                try:
-                    # send_mail(F'Вам поступила заявка от ***[[ {FooterFeedback_name} ]]*** с абонентским номером << {FooterFeedback_phone} >>', F'***[[ {FooterFeedback_name} ]]*** с абонентским номером << {FooterFeedback_phone} >> отправил Вам заявку на консультацию!', 'avitadentedgar@yandex.ru', ['garrip91@mail.ru'], fail_silently=False)
-                    send_mail(F'Вам поступила заявка от ***[[ {FooterFeedback_name} ]]*** с абонентским номером << {FooterFeedback_phone} >>', F'***[[ {FooterFeedback_name} ]]*** с абонентским номером << {FooterFeedback_phone} >> отправил (-а) Вам заявку на консультацию!', 'avitadentedgar@yandex.ru', ['avitadentedgar@yandex.ru'], fail_silently=False)
-                    print(send_mail)
-                except:
-                    return HttpResponseNotFound('<h1>Письмо не отправлено</h1>')
-                else:
-                    print("ВАША ЗАЯВКА УСПЕШНО ОТПРАВЛЕНА!")
-                    messages.success(request, "ВАША ЗАЯВКА УСПЕШНО ОТПРАВЛЕНА!")
-                    footer_feedback_form.save()
-                    return HttpResponseRedirect(F'{self.request.path}#thanks')
-            else:
-                print("ЧТО-ТО ПОШЛО НЕ ТАК!")
-                messages.error(request, 'НЕПРАВИЛЬНО ВВЕДЁН НОМЕР ТЕЛЕФОНА!')
-                return HttpResponseRedirect(self.request.path)
-        else:
-            self.footer_feedback_form = FooterFeedbackForm()
         #####################################################################################
         # print(F'request.path == {request.path}')
         return super().dispatch(request, *args, **kwargs)
--- src/TverWashProject/settings.py
+++ src/TverWashProject/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'TverWashApp',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
