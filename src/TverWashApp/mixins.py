44e005afe646d4d209a851c53ff38f719a8370d7
garrip91 <45920127+garrip91@users.noreply.github.com>
2022-05-07 20:41:01 +0300

    Summary

--- src/TverWashApp/mixins.py
+++ src/TverWashApp/mixins.py
@@ -15,23 +15,24 @@ class MyFormMixin1(View):
         if request.method == 'POST' and 'feedback1' in request.POST:
             call_request_form = CallRequestForm(request.POST)
             #print(call_request_form)
-            print(call_request_form.is_valid())
+            #print(call_request_form.is_valid())
             if call_request_form.is_valid():
                 CallRequestForm_name = call_request_form.cleaned_data.get('name')
                 #print(CallRequestForm_name)
                 CallRequestForm_phone = call_request_form.cleaned_data.get('phone')
                 #print(CallRequestForm_phone)
-#                try:
-                send_mail(F'Вам поступила заявка от ***[[ ТЕСТ ]]*** с абонентским номером << +7 (999) 999-99-99 >>', F'***[[ ТЕСТ ]]*** с абонентским номером << +7 (999) 999-99-99 >> отправил Вам заявку на консультацию!', 'sarkis.bazayan@yandex.ru', ['garrip91@mail.ru'], fail_silently=False)
-                print(send_mail)
-#                except:
-#                    return HttpResponseNotFound('<h1>Письмо не отправлено</h1>')
-#                else:
-                print("ВАША ЗАЯВКА УСПЕШНО ОТПРАВЛЕНА!")
-                messages.success(request, "ВАША ЗАЯВКА УСПЕШНО ОТПРАВЛЕНА!")
-                call_request_form.save()
-                #return HttpResponseRedirect(self.request.path)
-                return HttpResponseRedirect(F'{self.request.path}#thanks')
+                try:
+                # send_mail(F'Вам поступила заявка от ***[[ ТЕСТ ]]*** с абонентским номером << +7 (999) 999-99-99 >>', F'***[[ ТЕСТ ]]*** с абонентским номером << +7 (999) 999-99-99 >> отправил Вам заявку на консультацию!', 'sarkis.bazayan@yandex.ru', ['sarkis.bazayan@yandex.ru'], fail_silently=False)
+                    send_mail(F'Вам поступила заявка от ***[[ {CallRequestForm_name} ]]*** с абонентским номером << {CallRequestForm_phone} >>', F'***[[ {CallRequestForm_name} ]]*** с абонентским номером << {CallRequestForm_phone} >> отправил Вам заявку на запись на приём!', 'sarkis.bazayan@yandex.ru', ['sarkis.bazayan@yandex.ru'], fail_silently=False)
+                    print(send_mail)
+                except:
+                    return HttpResponseNotFound('<h1>Письмо не отправлено</h1>')
+                else:
+                    print("ВАША ЗАЯВКА УСПЕШНО ОТПРАВЛЕНА!")
+                    messages.success(request, "ВАША ЗАЯВКА УСПЕШНО ОТПРАВЛЕНА!")
+                    call_request_form.save()
+                    #return HttpResponseRedirect(self.request.path)
+                    return HttpResponseRedirect(F'{self.request.path}#thanks')
             else:
                 print("ЧТО-ТО ПОШЛО НЕ ТАК!")
                 messages.error(request, 'НЕПРАВИЛЬНО ВВЕДЁН НОМЕР ТЕЛЕФОНА!')
