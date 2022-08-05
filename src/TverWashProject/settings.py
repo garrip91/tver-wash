00d6c59e53a6157b6be1876d1220beae6612ee52
garrip91 <garrip91@gmail.com>
2022-08-05 13:56:45 +0300

    Summary

--- src/TverWashProject/settings.py
+++ src/TverWashProject/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import environ
 
 from . import Yandex_config
 
@@ -11,11 +12,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False),
+    EMAIL_PORT=(int, 0),
+    EMAIL_USE_TLS=(bool, False),
+    EMAIL_USE_SSL=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 
@@ -123,10 +134,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-EMAIL_BACKEND = Yandex_config.EMAIL_BACKEND
-EMAIL_HOST = Yandex_config.EMAIL_HOST
-EMAIL_PORT = Yandex_config.EMAIL_PORT
-EMAIL_HOST_USER = Yandex_config.EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD = Yandex_config.EMAIL_HOST_PASSWORD
-EMAIL_USE_TLS = Yandex_config.EMAIL_USE_TLS
-EMAIL_USE_SSL = Yandex_config.EMAIL_USE_SSL
+EMAIL_BACKEND = env('EMAIL_BACKEND')
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+EMAIL_USE_SSL = env('EMAIL_USE_SSL')
