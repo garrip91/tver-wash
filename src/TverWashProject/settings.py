e6dce408d99601f4b34f5c1ab4aedd6c365ad597
garrip91 <garrip91@gmail.com>
2022-08-05 11:47:31 +0300

    Summary

--- src/TverWashProject/settings.py
+++ src/TverWashProject/settings.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import os
 
 from . import Yandex_config
-from .SECRET_KEY import KEY
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -13,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = KEY
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 
