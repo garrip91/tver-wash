e173f2a0041b12682446a892784c8ca294ff2240
garrip91 <45920127+garrip91@users.noreply.github.com>
2022-10-27 21:13:05 +0300

    Delete Yandex_config.py

--- src/TverWashProject/Yandex_config.py
+++
@@ -1,7 +0,0 @@
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = "sarkis.bazayan"
-EMAIL_HOST_PASSWORD = "aanrgzqdilepvhcm"
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
